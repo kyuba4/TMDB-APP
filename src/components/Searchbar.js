4997ed218bdb4769e080e4ea2d9510df5c5e8323
Kuba Jagodzinski <jakub.jagodzinski@onet.pl>
2021-12-28 16:51:08 +0100

    Search bar component and styling

---
+++ src/components/Searchbar.js
@@ -0,0 +1,14 @@
+const Searchbar = () => {
+  return (
+    <div className=" flex items-center justify-center bg-slate-500 w-full h-20 shadow-md shadow-slate-500/70">
+      <input
+        type="text"
+        className="h-1/2 w-full max-w-5xl pl-5 mx-7 rounded-lg bg-slate-300 text-black border-0 outline-0 duration-300 ease focus:brightness-95"
+        placeholder="Search for a movie"
+        maxLength="65"
+      />
+    </div>
+  );
+};
+
+export default Searchbar;
--- src/views/Home.js
+++ src/views/Home.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import useFetchMovies from "../hooks/useFetchMovies";
 import FeaturedMovie from "../components/FeaturedMovie";
+import Searchbar from "../components/Searchbar";
 
 const Home = () => {
   const { data, pending } = useFetchMovies();
@@ -12,7 +13,13 @@ const Home = () => {
     }
   }, [data]);
 
-  return <>{movies && <FeaturedMovie props={movies.results[0]} />}</>;
+  return (
+    <>
+      {pending && <div>Loading...</div>}
+      {movies && <FeaturedMovie props={movies.results[0]} />}
+      {movies && <Searchbar />}
+    </>
+  );
 };
 
 export default Home;
