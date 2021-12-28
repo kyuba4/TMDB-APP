2ebfd34e3654de7bce26be2ba6866824420df578
Kuba Jagodzinski <jakub.jagodzinski@onet.pl>
2021-12-28 17:53:33 +0100

    Footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import Header from "./components/Header";
 import Home from "./views/Home";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Header />
       <Home />
+      <Footer />
     </>
   );
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+const Footer = () => {
+  return (
+    <div className="bg-slate-600 h-8 text-white flex items-center justify-center mt-8">
+      Website created by JJ&copy; using TMDB Api
+    </div>
+  );
+};
+
+export default Footer;
