9930abb1fae7180461d1e5f1466d88c0ded33a75
Kuba Jagodzinski <jakub.jagodzinski@onet.pl>
2022-01-03 20:22:57 +0100

    Title on movie card hover

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,10 +1,13 @@
 const MovieCard = ({ data }) => {
-  const { poster_path: poster } = data;
+  const { poster_path: poster, original_title, original_name } = data;
 
   const POSTER_IMG = poster ? `https://image.tmdb.org/t/p/original${poster}` : null;
 
   return (
-    <div className="flex flex-col items-center h-full max-w-xs duration-300 ease-in-out cursor-pointer hover:opacity-90 ">
+    <div
+      className="flex flex-col items-center h-full max-w-xs duration-300 ease-in-out cursor-pointer hover:opacity-90"
+      title={original_title || original_name}
+    >
       {poster && <img className="rounded-md w-full h-full shadow-md shadow-gray-500" src={POSTER_IMG} alt="Poster" />}
     </div>
   );
