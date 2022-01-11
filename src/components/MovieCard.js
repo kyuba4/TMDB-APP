34c98e798055727ac105aad15315c3e8a5fa9a85
Kuba Jagodzinski <jakub.jagodzinski@onet.pl>
2022-01-11 19:27:52 +0100

    Movie's images are now in lower resolutions which decreases their loading time

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -5,7 +5,7 @@ const MovieCard = ({ data }) => {
 
   // Take img from API if there isn't an image take a placehoder
   const POSTER_IMG = poster
-    ? `https://image.tmdb.org/t/p/original${poster}`
+    ? `https://image.tmdb.org/t/p/w500${poster}`
     : `https://plchldr.co/i/500x500?bg=000000&fc=3cff00&text=${original_title || title}`;
 
   const [imgLoad, setImgLoad] = useState(false);
@@ -13,7 +13,7 @@ const MovieCard = ({ data }) => {
   return (
     <div
       className={`${
-        imgLoad ? "entry-anim" : "opacity-0 translate-y-4"
+        imgLoad ? "entry-anim" : "opacity-0"
       } flex mx-2 flex-col items-center h-full max-w-xs duration-300 ease-in-out cursor-pointer hover:opacity-90`}
       title={title || original_title}
     >
