ae5af2d3bf64e7757685441c2496b67c5b84c5b1
Kuba Jagodzinski <jakub.jagodzinski@onet.pl>
2022-01-16 16:52:32 +0100

    Footer positioning fixed

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,7 @@
 const Footer = () => {
   return (
-    <footer>
-      <div className="bg-slate-600 h-8 text-white flex items-center justify-center w-full mt-8">
-        Website created by JJ&copy; using TMDB Api
-      </div>
+    <footer className="bg-slate-600 h-8 text-white flex items-center justify-center w-full mt-8">
+      Website created by JJ&copy; using TMDB Api
     </footer>
   );
 };
