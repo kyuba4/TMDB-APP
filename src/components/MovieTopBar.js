f12767d8387ba44a67eca7b9cd6ef27721698cec
Kuba Jagodzinski <jakub.jagodzinski@onet.pl>
2022-01-17 19:55:11 +0100

    Link to changed into button with useNavigate

--- src/components/MovieTopBar.js
+++ src/components/MovieTopBar.js
@@ -1,12 +1,14 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const MovieTopBar = ({ data }) => {
+  const navigate = useNavigate();
+
   return (
     <div className="bg-gradient-to-b from-slate-500 to-slate-400 p-5 flex items-center text-white text-sm lg:text-base">
       <div className="flex">
-        <Link className="hover:underline" to="/">
-          Home
-        </Link>
+        <button className="hover:underline" onClick={() => navigate(-1)}>
+          Go Back
+        </button>
         <div className="mx-3">|</div>
         <div>{data.title || data.original_title}</div>
       </div>
