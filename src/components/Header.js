ca500fb12b0848261b531078c868dabce37a68e2
Kuba Jagodzinski <jakub.jagodzinski@onet.pl>
2022-06-13 17:25:40 +0200

    New header and searchbar color

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className={styles.header}>
+    <div className={styles.header} style={{ backgroundColor: "rgba(0,0,0,0.8)", backdropFilter: "saturate(180%) blur(20px)" }}>
       <Link to="/">
         <img src={Logo} alt="tmdb" className={styles.img} />
       </Link>
@@ -12,7 +12,7 @@ const Header = () => {
 };
 
 const styles = {
-  header: "w-full bg-gradient-to-b from-slate-600 to-slate-500 flex p-5 justify-between items-center",
+  header: "fixed top-0 left-0 w-full flex h-16 px-5 justify-between items-center z-50",
   img: "max-h-8 w-full",
 };
 
--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,6 +1,6 @@
 const Searchbar = ({ searchTerm, setSearchTerm }) => {
   return (
-    <div className={styles.wrap}>
+    <div className={styles.wrap} style={{ backgroundColor: "rgba(0,0,0,0.8)" }}>
       <input
         type="text"
         className={styles.input}
@@ -14,7 +14,7 @@ const Searchbar = ({ searchTerm, setSearchTerm }) => {
 };
 
 const styles = {
-  wrap: "flex items-center justify-center bg-slate-500 w-full h-20 shadow-md shadow-slate-500/40",
+  wrap: "flex items-center justify-center w-full h-20 shadow-md shadow-zinc-500",
   input:
     "h-1/2 w-full max-w-5xl pl-5 mx-7 rounded-lg bg-slate-300 text-black border-0 outline-0 duration-300 ease focus:brightness-95",
 };
